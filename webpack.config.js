68fa883e8a6cd0d3549a887971c46f65dd59fdea
Ekaterina Ch <enc1@tpu.ru>
2017-04-01 16:30:01 +0700

    index.html (вместо home), отступ справа, некоторые иконки заголовков

--- webpack.config.js
+++ webpack.config.js
@@ -4,53 +4,53 @@ const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    context: __dirname + '/frontend',
-    entry: {
-        main: "./main.js"
-    },
+        context: __dirname + '/frontend',
+        entry: {
+            main: "./main.js"
+        },
 
-    output: {
-        path: __dirname + '/public/html',
-        filename: "home.js"
-    },
+        output: {
+            path: __dirname + '/public/html',
+            filename: "home.js"
+        },
 
-    watch: true,
+        watch: true,
 
-    watchOptions: {
-        aggregateTimeout: 100
-    },
+        watchOptions: {
+            aggregateTimeout: 100
+        },
 
-    plugins: [
-        new HtmlWebpackPlugin({
-            filename: 'home.html',
-            template: 'test.pug'
-        })
-    ],
+        plugins: [
+            new HtmlWebpackPlugin({
+                filename: 'index.html',
+                template: 'test.pug'
+            })
+        ],
 
-    resolve: {
-        modules: ['node_modules'],
-        extensions: ['*', '.js', '.pug', '.styl', 'css']
-    },
+        resolve: {
+            modules: ['node_modules'],
+            extensions: ['*', '.js', '.pug', '.styl', 'css']
+        },
 
-    resolveLoader: {
-       modules: ['node_modules'],
-       moduleExtensions: ['*-loader', '*'],
-       extensions: ['*', '.js']
-   },
+        resolveLoader: {
+            modules: ['node_modules'],
+            moduleExtensions: ['*-loader', '*'],
+            extensions: ['*', '.js']
+        },
 
-    module: {
-      loaders: [
-      {
-        test: /\.pug$/,
-        loader: 'pug-loader'
-      },
-      {
-        test: /\.styl$/,
-        loader: 'style-loader!css-loader!stylus-loader'
-      },
-      {
-            test:   /\.(css|png|jpg|svg|ttf|eot|woff|woff2)$/,
-            loader: 'file-loader?name=[name].[ext]'
-      }]
-  }
-};
+        module: {
+            loaders: [
+                {
+                    test: /\.pug$/,
+                    loader: 'pug-loader'
+                },
+                {
+                    test: /\.styl$/,
+                    loader: 'style-loader!css-loader!stylus-loader'
+                },
+                {
+                    test: /\.(css|png|jpg|svg|ttf|eot|woff|woff2)$/,
+                    loader: 'file-loader?name=[name].[ext]'
+                }]
+        }
+    };
